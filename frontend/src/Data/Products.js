a61e7993838c6d65aaa3b1a841f82a0b09b65b41
Namansh1 <rs0049614@gmail.com>
2026-06-09 15:31:16 +0530

    Update Products.js

--- frontend/src/Data/Products.js
+++ frontend/src/Data/Products.js
@@ -1,10 +1,13 @@
 export const fragrances = [
-  { id: 1, name: "Marine ", price: 99, image: "/images/Marine.jpg" },
-  { id: 2, name: "Ameer al Oud", price: 99, image: "/images/Ameeraloud.jpg" },
-  { id: 3, name: "Afterhours", price: 99, image: "/images/Afterhours.jpg" },
-  { id: 4, name: "Sapphire", price: 99, image: "/images/Sapphire.jpg" },
-  { id: 5, name: "Legacy 7", price: 99, image: "/images/Legacy 7.jpg" },
-  { id: 6, name: "Black Vanilla", price: 99, image: "/images/Black Vanilla.jpg" },
-  { id: 7, name: "Perfect", price: 99, image: "/images/Perfect.jpg" },
-  { id: 8, name: "Addiction", price: 99, image: "/images/addiction.jpg" }
+  { id: 1, name: "Marine Addiction", price: 99, image: "/images/marine.jpg" },
+  { id: 2, name: "Velvet Oud", price: 99, image: "/images/oud.jpg" },
+  { id: 3, name: "Citrus Noir", price: 99, image: "/images/citrus.jpg" },
+  { id: 4, name: "Amber Rose", price: 99, image: "/images/rose.jpg" },
+  { id: 5, name: "Ocean Mist", price: 99, image: "/images/ocean.jpg" },
+  { id: 6, name: "Midnight Musk", price: 99, image: "/images/musk.jpg" },
+  { id: 7, name: "Golden Saffron", price: 99, image: "/images/saffron.jpg" },
+  { id: 8, name: "White Jasmine", price: 99, image: "/images/jasmine.jpg" }
 ];
+
+// 👇 THIS IS THE FIX (MOST IMPORTANT)
+export const products = fragrances;
