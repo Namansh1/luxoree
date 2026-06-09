e50198a1bc3969756304459702a9930e52e29162
Namansh1 <rs0049614@gmail.com>
2026-06-09 15:37:23 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ export default function App() {
         <Navbar />
 
         <Routes>
+          <Route path="/build-set" element={<SetBuilder />} />
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/cart" element={<Cart />} />
