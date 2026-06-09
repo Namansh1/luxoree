5d46c5b5e0246632ab7f00c5ed27eff9971732be
Namansh1 <rs0049614@gmail.com>
2026-06-09 15:38:59 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./styles/style.css"
+import { BrowserRouter } from "react-router-dom";
 
+<BrowserRouter>
+  <App />
+</BrowserRouter>
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
