05fc8566f0d7105f355cc02f6530ae855b31bf8c
Namansh1 <rs0049614@gmail.com>
2026-06-09 14:17:48 +0530

    force add frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import CartProvider from "./Context/CartContext";
+
+export default function App() {
+  return (
+    <CartProvider>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
+  );
+}
