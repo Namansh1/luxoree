13b514e4cc1c14e131ce948dc36a3880f5186859
Namansh1 <rs0049614@gmail.com>
2026-06-09 15:24:35 +0530

    Create sets.js

---
+++ frontend/src/Data/sets.js
@@ -0,0 +1,16 @@
+export const sets = [
+  {
+    id: "set20",
+    name: "20ml Duo Set",
+    size: "20ml",
+    maxSelect: 2,
+    price: 349
+  },
+  {
+    id: "set50",
+    name: "50ml Signature Bottle",
+    size: "50ml",
+    maxSelect: 1,
+    price: 399
+  }
+];
